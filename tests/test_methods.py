5cdee5a95eb35d63acb09b699677add992eaeed6
Fabian <fabian.terhorst@gmail.com>
2023-12-21 00:03:54 +0100

    improve tests

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -5,7 +5,7 @@
 """This is a sample python file for testing functions from the source code."""
 from __future__ import annotations
 
-from python_package.hello_world import everhome
+from everhome.everhome_client import everhome_client
 
 
 def hello_test():
@@ -13,4 +13,4 @@ def hello_test():
     This defines the expected usage, which can then be used in various test cases.
     Pytest will not execute this code directly, since the function does not contain the suffex "test"
     """
-    1 + 1
+    everhome_client().test()
